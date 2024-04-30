cca1936228b738f9f94415f4b8c8d4693fe7e4e1
Ashik Hasan <120168546+Ashik-gf@users.noreply.github.com>
2024-04-30 23:24:43 +0600

    uplode this

---
+++ slik.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.flash_Sales').slick({
+        // autoplay:true,
+        // autoplaySpeed:1000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:true,
+        nextArrow:".nextArrow",
+        prevArrow:".prevArrow"
+
+    });
+  });
