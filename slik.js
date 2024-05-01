e265520204f6632d1f8ad3400cdce114f4307358
Ashik Hasan <120168546+Ashik-gf@users.noreply.github.com>
2024-05-01 09:46:08 +0600

    try to do best for to day

--- slik.js
+++ slik.js
@@ -8,5 +8,29 @@ $(document).ready(function(){
         nextArrow:".nextArrow",
         prevArrow:".prevArrow"
 
-    });
+    }),
+    $('.Category_browser').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows:true,
+        nextArrow:".nextBtn",
+        prevArrow:".prevBtn",
+        centerMode: true,
+        dots: true,
+        mobileFirst:true,
+
+      });
+    $('.products_card').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows:true,
+        nextArrow:".btn_next",
+        prevArrow:".btn_prev",
+        centerMode: true,
+        dots: true,
+        rows:1,
+
+      });
+    
+    
   });
